0f04941ee21f7ecc9362f1b03c1e3757d90c0f82
vnnsnnt <vnnsnnt@gmail.com>
2020-11-21 14:45:20 -0800

    implemented fishing bot

--- control.py
+++ control.py
@@ -2,28 +2,19 @@ import pyautogui as bot
 import math 
 import random 
 import time 
-from fishing import Fisher
+import fishing
 
 #images
-x_mark_area = './clientImages/x_mark.jpg'
-world_switcher_area = './clientImages/world_switcher.jpg'
-inventory_closed = './clientImages/inventory.jpg'
-inventory_open = './clientImages/inventory_open.jpg' 
-
 running = True
-tick = 0.5
+tick = 0.3
 
 class Client: 
     def __init__(self, epoch=0, x_mark=[], world_switcher=[], inventory=[]):
         self.epoch = epoch 
-        self.x_mark = x_mark
-        self.world_switcher = world_switcher
-        self.inventory = inventory
-        self.fisher = Fisher()
-    
+        self.fisher = fishing.Fisher()
+
     def calibrate(self):
-        self.x_mark = bot.locateCenterOnScreen(x_mark_area, confidence=0.7)
-        self.world_switcher = bot.locateCenterOnScreen(world_switcher_area, confidence=0.8)
+        pass
 
     def elapseTime(self):
         self.epoch = round(time.time() - self.epoch, 2)
@@ -33,7 +24,7 @@ client = Client()
 while True: 
     time.sleep(tick)
     client.fisher.fish()
-
+    client.calibrate()
 
 
 
--- fishing.py
+++ fishing.py
@@ -1,59 +1,82 @@
 import pyautogui as bot 
 import time
-import math 
-import random
-tick = 0.3
-shrimp_area = './fishImages/shrimp_area.jpg'
-net_item = './fishImages/net.jpg'
-is_fishing_cue = './fishImages/is_fishing.jpg'
-full_cue = './fishImages/full_fishes.jpg'
-not_fishing_cue = './fishImages/not_fishing.jpg'
-inventory_closed = './clientImages/inventory.jpg'
-inventory_open = './clientImages/inventory_open.jpg' 
+mouseSpeed = 0.3
+
+flyFishing = ['./fishImages/flyFishing.jpg', './fishImages/flyFishing2.jpg', './fishImages/flyFishing3.jpg']
+netFishing = ['./fishImages/netFishing.jpg']
+fullIndicator = './fishImages/fullIndicator.jpg'
+levelUpIndicator = './fishImages/levelUpIndicator.jpg'
+notFishing = './fishImages/notFishing.jpg'
+inventoryOpen = './fishImages/inventoryOpen.jpg'
+inventoryClosed = './fishImages/inventoryClosed.jpg'
+inventorySalmon = './fishImages/inventorySalmon.jpg'
+inventoryTrout = './fishImages/inventoryTrout.jpg'
+inventoryShrimp = './fishImages/inventoryShrimp.jpg'
 
 class Fisher:
-    def __init__(self, fishingSpot={}, tool=[], isFishing=False, isFull=False, hasFishingSpot=False):
-        self.fishingSpot = fishingSpot
-        self.tool = tool 
+    def __init__(self, fishingSpot=[], isFishing=False, isFull=False, hasFishingSpot=False, currentFishing=flyFishing, fishes=[inventorySalmon, inventoryTrout]):
+        self.fishingSpot = fishingSpot 
         self.isFishing = isFishing
         self.isFull = isFull 
-        self.hasFishingSpot = hasFishingSpot
-        self.isInventoryOpen = False 
-    
-    def calibrate(self):
-        if not self.isFishing:
-            if bot.locateOnScreen(full_cue, confidence=0.7) is not None: 
-                print('Full on fishes!')
+        self.hasFishingSpot = hasFishingSpot 
+        self.currentFishing = currentFishing 
+        self.fishes = fishes
+
+    def calibrate(self): 
+        if not self.isFishing: 
+            if bot.locateOnScreen(fullIndicator, confidence=0.7) is not None: #check if inventory full 
+                print('Full on fishes!') #if fullIndicator is found 
                 self.isFull = True 
-            else:
-                inventory_area = bot.locateOnScreen(inventory_open, confidence=0.95) 
-                if inventory_area is not None: 
-                    bot.moveTo(inventory_area[0], inventory_area[1], tick)
-                    bot.click()
-                    self.isInventoryOpen = False 
-                print('Looking for a fishing spot!')
-                self.fishingSpot['spot'] = bot.locateCenterOnScreen(shrimp_area, confidence=0.7)
-                if self.fishingSpot['spot'] is not None:
-                    print('Found a fishing spot!') 
-                    self.hasFishingSpot = True
-                else:
+            elif bot.locateOnScreen(levelUpIndicator, confidence=0.7) is not None: #check if leveled up 
+                bot.moveTo(bot.locateCenterOnScreen(levelUpIndicator, confidence=0.7)[0], bot.locateCenterOnScreen(levelUpIndicator, confidence=0.7)[1], mouseSpeed)
+                bot.click() 
+            else: 
+                if bot.locateCenterOnScreen(inventoryOpen, confidence=0.9) is not None: 
+                    bot.moveTo(bot.locateCenterOnScreen(inventoryOpen, confidence=0.6)[0], bot.locateCenterOnScreen(inventoryOpen, confidence=0.6)[1], mouseSpeed) 
+                    bot.click() 
+                print('Looking for a fishing spot!') 
+                for images in self.currentFishing: 
+                    print('trying image', images)
+                    self.fishingSpot = bot.locateCenterOnScreen(images, confidence=0.6) #finds the fishing area
+                    if self.fishingSpot is not None: #if fishing area is found 
+                        print('Found a fishing spot!') 
+                        self.hasFishingSpot = True 
+                        break
+                else: 
                     print('Failed to find a fishing spot!')
+                    
         
-    def fish(self): 
-        self.calibrate()
-        if not self.isFishing and self.hasFishingSpot and not self.isFull:
-            bot.moveTo(self.fishingSpot['spot'][0], self.fishingSpot['spot'][1], tick)
+    def fish(self):
+        self.calibrate() 
+        if not self.isFishing and self.hasFishingSpot and not self.isFull and self.fishingSpot is not None: 
+            bot.moveTo(self.fishingSpot[0], self.fishingSpot[1], mouseSpeed)
             bot.click() 
             self.isFishing = True 
-            print('Started Fishing') 
-        if self.isFishing: 
-            time.sleep(7)
-            if bot.locateOnScreen(not_fishing_cue, confidence=0.7) is not None:
-                self.isFishing = False 
-                print('Stopped Fishing!')
-        if self.isFull: 
-            pass
-            
+            print('Started fishing!') 
 
-            
-           
+        if self.isFishing:
+            time.sleep(6) #checks every for fishing on an interval 
+            if bot.locateOnScreen(notFishing, confidence=0.6) is not None: 
+                self.isFishing = False 
+                print('Stopped fishing!') 
+        
+        if self.isFull: 
+            print('Dropping inventory!')
+            try:  
+                if bot.locateCenterOnScreen(inventoryClosed, confidence=0.9) is not None:
+                    bot.moveTo(bot.locateCenterOnScreen(inventoryClosed, confidence=0.8)[0], bot.locateCenterOnScreen(inventoryClosed, confidence=0.9)[1], mouseSpeed)
+                    bot.click() 
+            except: 
+                pass
+            bot.keyDown('shift') 
+            for fish in self.fishes: 
+                for item in bot.locateAllOnScreen(fish, confidence=0.85): 
+                    bot.moveTo(item[0]+15, item[1]+15, mouseSpeed) 
+                    bot.click() 
+                print('Dropped all of %s' % fish)
+            bot.keyUp('shift') 
+            self.isFull = False 
+
+
+    
+
